abf7752272b2ec06643de5146b21017356d5dc87
sundarkumarsatyala <sundarkumarsatyala@gmail.com>
2021-05-29 20:55:29 +0530

    footer addition4

--- src/components/Footer_SSS.js
+++ src/components/Footer_SSS.js
@@ -32,8 +32,6 @@ function Footer_SSS() {
                     <p class="social text-muted mb-0 pb-0 bold-text"> <span class="mx-2"><i class="fa fa-facebook" aria-hidden="true"></i></span> <span class="mx-2"><i class="fa fa-linkedin-square" aria-hidden="true"></i></span> <span class="mx-2"><i class="fa fa-twitter" aria-hidden="true"></i></span> <span class="mx-2"><i class="fa fa-instagram" aria-hidden="true"></i></span> </p><small class="rights"><span>&#174;</span> Sowka Smart Schools 2021, All Rights Reserved.</small>
                 </div>
                 <div class="col-xl-4 col-md-4 col-sm-4 col-auto order-1 align-self-end ">
-                <h6 class="mt-55 mt-2 text-muted bold-text"><b>N RAM MOHAN</b>&nbsp;-&nbsp;Managing Director</h6>
-                <h6 class="mt-55 mt-2 text-muted bold-text"><b>K SOWMYA LAKSHMI</b>&nbsp;-&nbsp;Head Teacher</h6>
                     <small> 
                         <span><i class="fa fa-envelope" aria-hidden="true"></i></span> 
                         <b>Email:</b>&nbsp;<a style={{color: '#627482'}} href="mailto:sowkasmartschools@gmail.com">sowkasmartschools@gmail.com</a>
